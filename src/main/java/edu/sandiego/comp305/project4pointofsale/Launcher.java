e8b6fd12e001716c77d7ca432336e345bf1614dc
ealamat <ealamat@sandiego.edu>
2026-04-27 10:22:36 -0700

    initial commit

---
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/Launcher.java
@@ -0,0 +1,9 @@
+package edu.sandiego.comp305.project4pointofsale;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module edu.sandiego.comp305.project4pointofsale {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens edu.sandiego.comp305.project4pointofsale to javafx.fxml;
+    exports edu.sandiego.comp305.project4pointofsale;
+}
