9134ea87197a1af5bdcd22a62c231f1be513a0fe
Aiden Domingo <adomingo@sandiego.edu>
2026-04-29 10:23:27 -0700

    feat: setup methods with objects instantiated and test classes defined initially, also started a BeforeAll method to create a consistent sample inventory for each test

--- src/test/java/InventoryTests.java
+++ src/test/java/InventoryTests.java
@@ -1,14 +1,60 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryTests {
+    private static List<Ingredient> sampleInventory;
+
+    @BeforeAll
+    public static void makeSampleInventory(){
+        sampleInventory = new ArrayList<>();
+
+
+    }
+
     @Test
     public void checkInventoryTest(){
+        List<Ingredient> ingredientsContained = sampleInventory;
+        List<Ingredient> restockIngredients = new ArrayList<>();
+
+        Inventory inv = new Inventory(ingredientsContained, restockIngredients);
+
 
     }
 
     @Test
     public void restockInventoryTest(){
+        List<Ingredient> ingredientsContained = sampleInventory;
+        List<Ingredient> restockIngredients = sampleInventory;
+
+        Inventory inv = new Inventory(ingredientsContained, restockIngredients);
+    }
+
+    @Test
+    public void restockNothingTest(){
+        List<Ingredient> ingredientsContained = sampleInventory;
+        List<Ingredient> restockIngredients = new ArrayList<>();
+
+        Inventory inv = new Inventory(ingredientsContained, restockIngredients);
+    }
+
+    @Test
+    public void checkEmptyInventoryTest(){
+        List<Ingredient> ingredientsContained = new ArrayList<>();
+        List<Ingredient> restockIngredients = new ArrayList<>();
+
+        Inventory inv = new Inventory(ingredientsContained, restockIngredients);
+    }
+
+    @Test
+    public void initializeEmptyParamsTest(){
+        List<Ingredient> ingredientsContained = new ArrayList<>();
+        List<Ingredient> restockIngredients = new ArrayList<>();
+
+        Inventory inv = new Inventory(ingredientsContained, restockIngredients);
 
     }
 }
