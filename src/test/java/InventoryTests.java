3cd02eaf2bda0fccb66d0fe853950e3a43c2c265
Aiden Domingo <adomingo@sandiego.edu>
2026-04-29 10:03:43 -0700

    feat: created method stubs for test class

---
+++ src/test/java/InventoryTests.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class InventoryTests {
+    @Test
+    public void checkInventoryTest(){
+
+    }
+
+    @Test
+    public void restockInventoryTest(){
+
+    }
+}
