76067d79a6b9d20662c91659271196c28daa38da
ealamat <ealamat@sandiego.edu>
2026-05-01 09:44:03 -0700

    order checkstyles fixed

--- src/main/java/edu/sandiego/comp305/project4pointofsale/MenuItem.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/MenuItem.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public abstract class MenuItem {
 
+
     String description;
 
     List<Ingredient> ingredients;
--- src/main/java/edu/sandiego/comp305/project4pointofsale/Order.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/Order.java
@@ -3,11 +3,11 @@ package edu.sandiego.comp305.project4pointofsale;
 public class Order {
     private final int orderId;
 
-    public Order(int orderId){
+    public Order(final int orderId){
         this.orderId = orderId;
     }
 
-    public void addItem(MenuItem item){}
+    public void addItem(final MenuItem item){}
 
     public double calculateTotal(){
         return 0;
