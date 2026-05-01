26075db8813a6c8d98ed214cf4c1a673b8469026
ealamat <ealamat@sandiego.edu>
2026-05-01 09:45:25 -0700

    POSSystem checkstyles fixed

--- src/main/java/edu/sandiego/comp305/project4pointofsale/POSSystem.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/POSSystem.java
@@ -1,11 +1,11 @@
 package edu.sandiego.comp305.project4pointofsale;
 
 public class POSSystem {
-    public void placeOrder(Order order){}
+    public void placeOrder(final Order order){}
 
-    public KitchenTicket generateTicket(Order order){
+    public KitchenTicket generateTicket(final Order order){
         return null;
     }
 
-    public void updateInventory(Order order){}
+    public void updateInventory(final Order order){}
 }
