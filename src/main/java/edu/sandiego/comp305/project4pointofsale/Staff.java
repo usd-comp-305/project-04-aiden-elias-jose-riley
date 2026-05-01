05a8f02b583992e0beded2be323befa91186bc71
Aiden Domingo <adomingo@sandiego.edu>
2026-05-01 10:08:14 -0700

    chore: fixing spotbug check in Staff class

--- src/main/java/edu/sandiego/comp305/project4pointofsale/Staff.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/Staff.java
@@ -2,9 +2,10 @@ package edu.sandiego.comp305.project4pointofsale;
 
 public abstract class Staff {
 
-    private int id;
+    private int id = 0;
 
-    private String name;
+    private String name = null;
 
     public abstract boolean login();
+
 }
