0147ec64b9a9173454c552e794def7c94f22a184
joseantoniojaga <joseantoniojaga@hotmail.com>
2026-05-07 09:55:42 -0700

    feat: add MobilePayment class and stubs

---
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/MobilePayment.java
@@ -0,0 +1,18 @@
+package edu.sandiego.comp305.project4pointofsale;
+
+public class MobilePayment implements PaymentMethod {
+    private String provider;
+
+    public MobilePayment(String provider) {
+        this.provider = provider;
+    }
+
+    @Override
+    public boolean pay(double amount) {
+        return false;
+    }
+
+    public boolean verifyAccount() {
+        return false;
+    }
+}
