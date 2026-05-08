bc82b1dd9779249ab0242e9c1f6ac6ded9897095
Riley Miller <rileymiller@sandiego.edu>
2026-05-08 10:21:05 -0700

    implemented gluten free food classes

--- src/main/java/edu/sandiego/comp305/project4pointofsale/GlutenFreePasta.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/GlutenFreePasta.java
@@ -5,17 +5,10 @@ import java.util.List;
 public class GlutenFreePasta extends MenuItem {
     public GlutenFreePasta(final List<Ingredient> ingredients){
         super(ingredients);
+        this.description = "Tender gluten-free pasta tossed with " +
+                "fresh ingredients and a flavorful house sauce.\n";
+        this.cost = 15.99;
     }
 
     public GlutenFreePasta(){}
-
-    @Override
-    public String getDescription() {
-        return "";
-    }
-
-    @Override
-    public double getCost() {
-        return 0;
-    }
 }
