08272133e31a4dd10206df97c8a3016786489860
ealamat <ealamat@sandiego.edu>
2026-05-11 14:42:09 -0700

    updated staff test

--- src/test/java/edu.sandiego.comp305.project4pointofsale/StaffTest.java
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/StaffTest.java
@@ -43,29 +43,4 @@ class StaffTest {
                 .defaultAnswer(Mockito.CALLS_REAL_METHODS));
         assertEquals("Alex", elias.getName());
     }
-
-    @Test
-    void setId() {
-        final Staff elias = Mockito.mock(Staff.class, Mockito.withSettings()
-                .useConstructor(42, "Alex", "password")
-                .defaultAnswer(Mockito.CALLS_REAL_METHODS));
-
-        elias.setId(1);
-
-        assertEquals(1, elias.getId());
-
-    }
-
-    @Test
-    void setName() {
-        final Staff elias = Mockito.mock(Staff.class, Mockito.withSettings()
-                .useConstructor(42, "Alex", "password")
-                .defaultAnswer(Mockito.CALLS_REAL_METHODS));
-
-        elias.setName("Elias");
-
-        assertEquals("Elias", elias.getName());
-
-
-    }
 }
