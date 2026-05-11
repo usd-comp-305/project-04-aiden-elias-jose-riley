24c425ebe18aa55fdea45d5a1e61b73ad542c906
ealamat <ealamat@sandiego.edu>
2026-05-11 14:01:11 -0700

    added test file

---
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/ServerTest.java
@@ -0,0 +1,21 @@
+package edu.sandiego.comp305.project4pointofsale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @Test
+    void viewTickets() {
+
+    }
+
+    @Test
+    void updateOrderStatus() {
+    }
+
+    @Test
+    void checkInventory() {
+    }
+}
