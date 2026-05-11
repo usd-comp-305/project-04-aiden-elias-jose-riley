92ab8f883b08f575dc020a267b444a6541b20e48
ealamat <ealamat@sandiego.edu>
2026-05-11 14:55:16 -0700

    staff repository stubs done

--- src/main/java/edu/sandiego/comp305/project4pointofsale/StaffRepository.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/StaffRepository.java
@@ -1,4 +1,27 @@
 package edu.sandiego.comp305.project4pointofsale;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StaffRepository {
+
+    private final List<Staff> staffMembers;
+
+    public StaffRepository(){
+        this.staffMembers = new ArrayList<>();
+    }
+
+    public void loadDefaultStaff(){
+
+    }
+
+    public Staff findByPin(final String pin){
+        return null;
+    }
+
+    public int getStaffCount(){
+        return 0;
+    }
+
+
 }
