1da7d0c29ac05c76159fffe804fc1d311b7a8c02
ealamat <ealamat@sandiego.edu>
2026-05-12 09:44:38 -0700

    loginService stubs

---
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/LoginService.java
@@ -0,0 +1,13 @@
+package edu.sandiego.comp305.project4pointofsale;
+
+public class LoginService {
+    private final StaffRepository staffRepo;
+
+    public LoginService(StaffRepository staffRepo) {
+        this.staffRepo = staffRepo;
+    }
+
+    public Staff login(final String password){
+        return null;
+    }
+}
