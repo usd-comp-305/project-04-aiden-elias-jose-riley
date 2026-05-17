330f95bed5d18846bd1d1efe25e4099458d28fd6
ealamat <ealamat@sandiego.edu>
2026-05-17 16:35:47 -0700

    ingredient stubs finalized

--- src/main/java/edu/sandiego/comp305/project4pointofsale/Ingredient.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/Ingredient.java
@@ -2,13 +2,13 @@ package edu.sandiego.comp305.project4pointofsale;
 
 public class Ingredient {
 
-    public String name;
+    public final String name;
 
     private int quantity;
 
-    public Ingredient(){
-        name = "";
-        quantity = 0;
+    public Ingredient(final String name, final int quantity){
+        this.name = name;
+        this.quantity = quantity;
     }
 
     public String getName() {
@@ -18,4 +18,6 @@ public class Ingredient {
     public int getQuantity() {
         return quantity;
     }
+
+
 }
