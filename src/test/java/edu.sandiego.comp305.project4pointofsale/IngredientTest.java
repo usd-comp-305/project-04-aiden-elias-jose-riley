c138eb30a3597c428e15065fdda15f9a1778abde
ealamat <ealamat@sandiego.edu>
2026-05-17 16:36:36 -0700

    ingredient test file added

---
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/IngredientTest.java
@@ -0,0 +1,17 @@
+package edu.sandiego.comp305.project4pointofsale;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientTest {
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getQuantity() {
+    }
+}
