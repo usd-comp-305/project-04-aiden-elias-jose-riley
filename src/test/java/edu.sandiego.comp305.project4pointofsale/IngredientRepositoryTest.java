0259927b36f64810e84cd50a50375a9cdddd01c8
Aiden Domingo <adomingo@sandiego.edu>
2026-05-18 11:55:11 -0700

    feat: implemented tests for expected behavior of IngredientRepository

---
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/IngredientRepositoryTest.java
@@ -0,0 +1,35 @@
+package edu.sandiego.comp305.project4pointofsale;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IngredientRepositoryTest {
+    
+    private static IngredientRepository ingredientRepository = null;
+
+    @BeforeAll
+    static void setup(){
+        ingredientRepository = new IngredientRepository();
+    }
+
+    @Test
+     void getPriceOfIngredientInRepo() {
+        double storedPrice = ingredientRepository.getPrice("steak");
+
+        assertEquals(15, storedPrice);
+    }
+
+    @Test
+     void getPriceOfIngredientNotInRepo() {
+        assertThrows(IllegalArgumentException.class, () -> ingredientRepository.getPrice("a"));
+    }
+
+    @Test
+    void getIngredientInRepo() {
+        Ingredient testSteak = new Ingredient("steak", 15);
+
+        assertEquals(testSteak,ingredientRepository.getIngredient("steak"));
+    }
+}
