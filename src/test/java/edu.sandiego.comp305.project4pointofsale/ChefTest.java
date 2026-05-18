6ef57d8a94586f4665b0503f9167eb1e666d87fd
ealamat <ealamat@sandiego.edu>
2026-05-17 20:58:55 -0700

    chef tests done

--- src/test/java/edu.sandiego.comp305.project4pointofsale/ChefTest.java
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/ChefTest.java
@@ -14,10 +14,48 @@ class ChefTest {
     }
 
     @Test
-    void createOrder() {
+    void startTicket() {
+        final Chef chef = new Chef(22, "Elias", "hello2");
+
+        final Server server = new Server(1, "EliasServerVersion", "1234");
+
+        Order order = new Order(30, server);
+
+        KitchenTicket ticket = new KitchenTicket(501, order);
+
+        chef.startTicket(ticket);
+
+        assertEquals(Status.IN_PROCESS, ticket.getStatus());
     }
 
     @Test
-    void sendOrder() {
+    void markTicketReady() {
+        final Chef chef = new Chef(22, "Elias", "hello2");
+
+        final Server server = new Server(1, "EliasServerVersion", "1234");
+
+        Order order = new Order(30, server);
+
+        KitchenTicket ticket = new KitchenTicket(501, order);
+
+        chef.markTicketReady(ticket);
+
+        assertEquals(Status.COMPLETED, ticket.getStatus());
+    }
+
+    @Test
+    void markTicketDelivered() {
+        final Chef chef = new Chef(22, "Elias", "hello2");
+
+        final Server server = new Server(1, "EliasServerVersion", "1234");
+
+        Order order = new Order(30, server);
+
+        KitchenTicket ticket = new KitchenTicket(501, order);
+
+        chef.markTicketDelivered(ticket);
+
+        assertEquals(Status.DELIVERED, ticket.getStatus());
+
     }
 }
