10a4b6a2adf6e096e2adfcf9e62ab73249677fbc
Riley Miller <rileymiller@sandiego.edu>
2026-05-17 17:48:44 -0700

    test: more tests added

--- src/test/java/edu.sandiego.comp305.project4pointofsale/RegularMealFactoryTest.java
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/RegularMealFactoryTest.java
@@ -7,10 +7,26 @@ import static org.junit.jupiter.api.Assertions.*;
 public class RegularMealFactoryTest {
 
     @Test
-    public void factoryCreatesCorrectIngredients(){
+    public void factoryCreatesCorrectBurger(){
         RegularMealFactory factory = new RegularMealFactory();
         MenuItem burger = factory.createBurger();
 
         assertEquals(new RegularBurger().getIngredients(), burger.getIngredients());
+    }
+
+    @Test
+    public void factoryCreatesCorrectPasta(){
+        RegularMealFactory factory = new RegularMealFactory();
+        MenuItem pasta = factory.createPasta();
+
+        assertEquals(new RegularPasta().getIngredients(), pasta.getIngredients());
+    }
+
+    @Test
+    public void factoryCreatesCorrectSteak(){
+        RegularMealFactory factory = new RegularMealFactory();
+        MenuItem steak = factory.createSteak();
+
+        assertEquals(new RegularSteak().getIngredients(), steak.getIngredients());
     }
 }
