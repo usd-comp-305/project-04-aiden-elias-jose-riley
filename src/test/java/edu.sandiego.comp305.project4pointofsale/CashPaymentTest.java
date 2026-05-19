d408ed00bcfd6d4d467a73c70f3b1664ff57505e
Aiden Domingo <adomingo@sandiego.edu>
2026-05-19 07:57:12 -0700

    fix: removed tests that relied on Inventory being income management

--- src/test/java/edu.sandiego.comp305.project4pointofsale/CashPaymentTest.java
+++ src/test/java/edu.sandiego.comp305.project4pointofsale/CashPaymentTest.java
@@ -12,7 +12,6 @@ public class CashPaymentTest {
         final boolean result = payment.pay(15.00);
 
         assertTrue(result);
-        Inventory.resetIncome();
     }
 
     @Test
@@ -22,7 +21,6 @@ public class CashPaymentTest {
         final boolean result = payment.pay(15.00);
 
         assertFalse(result);
-        Inventory.resetIncome();
     }
 
     @Test
@@ -32,26 +30,5 @@ public class CashPaymentTest {
         payment.pay(15.00);
 
         assertEquals(5.00, payment.calculateChange());
-        Inventory.resetIncome();
-    }
-
-    @Test
-    public void incomeIncreasesAfterSuccessfulCashPayment() {
-        final CashPayment payment = new CashPayment(20.00);
-
-        payment.pay(15.00);
-
-        assertEquals(15.00, Inventory.getIncome());
-        Inventory.resetIncome();
-    }
-
-    @Test
-    public void incomeDoesNotIncreaseAfterFailedCashPayment() {
-        final CashPayment payment = new CashPayment(10.00);
-
-        payment.pay(15.00);
-
-        assertEquals(0.00, Inventory.getIncome());
-        Inventory.resetIncome();
     }
 }
