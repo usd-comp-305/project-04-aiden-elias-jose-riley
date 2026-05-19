c43748c5c224b0f6a4633f305feb0562301e01f3
joseantoniojaga <joseantoniojaga@hotmail.com>
2026-05-18 20:03:10 -0700

    feat: implemented ActiveOrderController

--- src/main/java/edu/sandiego/comp305/project4pointofsale/ActiveOrderController.java
+++ src/main/java/edu/sandiego/comp305/project4pointofsale/ActiveOrderController.java
@@ -1,4 +1,22 @@
 package edu.sandiego.comp305.project4pointofsale;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
+
 public class ActiveOrderController {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private void handleBack() throws IOException{
+        final Stage stage = (Stage) root.getScene().getWindow();
+        SceneManager.switchScene(stage, "server-view.fxml",
+                "Server Dashboard");
+    }
 }
